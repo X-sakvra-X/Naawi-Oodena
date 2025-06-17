79ae37c26dafa0a918a8bac98fc8d9a1b1f9bfc8
X-sakvra-X <appleboosimp1@gmail.com>
2025-06-16 19:53:08 -0500

    AAAAAAAAHHHHHHHHHHHH

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -5,3 +5,9 @@ const hamburger = document.getElementById('hamburger');
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
+
+    document.getElementById('signupForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      alert("Your submission has been received! Thank you for joining Naawi-Oodena 🌿");
+      this.reset(); // Optional: Clears the form after submission
+    });
